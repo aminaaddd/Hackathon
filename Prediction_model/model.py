21abae5422da67e268243d92a9c30293083dae6f
ad.m.na <amina.addi@yahoo.com>
2025-09-18 18:54:47 +0200

    Clean the script.

--- Prediction_model/model.py
+++ Prediction_model/model.py
@@ -17,8 +17,6 @@ def train_model_classification(X, y):
     y_pred = clf.predict(X_test)
     return y_pred, y_test, clf, X_test
 
-#print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
-#print("\nClassification Report:\n", classification_report(y_test, y_pred, target_names=["Worse","Stable","Better"]))
 
 
 def train_model_with_weights(
@@ -33,7 +31,6 @@ def train_model_with_weights(
         X, y.astype(int), test_size=test_size, random_state=random_state, stratify=y
     )
 
-    # OPTION A (simple): class_weight explicite
     clf = RandomForestClassifier(
         n_estimators=n_estimators,
         class_weight={0: 1.0, 1: float(weight_pos)}, 
